1a9ec8601017e316505d538938d5174befea9d8a
gee03143 <51311680+gee03143@users.noreply.github.com>
2026-04-04 20:13:39 +0900

    엔진 버전 변경, 의존성 모듈 추가
    엔진 버전 :  5.4 -> 5.7
    의존성 모듈 추가 "NetCore", "Networking"

--- Source/Vrem/Vrem.Build.cs
+++ Source/Vrem/Vrem.Build.cs
@@ -8,7 +8,7 @@ public class Vrem : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "GameplayTags" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "GameplayTags", "NetCore", "Networking" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
