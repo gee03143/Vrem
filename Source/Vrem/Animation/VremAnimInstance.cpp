023f58c009cd88c0fd55b3e589512d67a7054bf8
gee03143 <51311680+gee03143@users.noreply.github.com>
2026-05-12 12:24:30 +0900

    태그 유무를 AnimBP에서 Bool값 플래그로 바인딩할 수 있도록 기능 추가
    * VremCharacter 의존성이 추가되었는데, 추후 별도 컴포넌트나 ASC로 로직 이주 가능하면 옮길 것

--- Source/Vrem/Animation/VremAnimInstance.cpp
+++ Source/Vrem/Animation/VremAnimInstance.cpp
@@ -2,3 +2,90 @@
 
 
 #include "VremAnimInstance.h"
+#include "Vrem/Character/VremCharacter.h"
+#include "Vrem/VremLogChannels.h"
+
+void UVremAnimInstance::NativeInitializeAnimation()
+{
+	Super::NativeInitializeAnimation();
+	CacheTagBindingProperties();
+}
+
+void UVremAnimInstance::NativeBeginPlay()
+{
+	Super::NativeBeginPlay();
+
+	APawn* PawnOwner = TryGetPawnOwner();
+	if (!IsValid(PawnOwner))
+	{
+		UE_LOG(LogVremInput, Warning, TEXT("UVremAnimInstance::NativeBeginPlay invalid PawnOwner"));
+		return;
+	}
+
+	AVremCharacter* VremChar = Cast<AVremCharacter>(PawnOwner);
+	if (IsValid(VremChar))
+	{
+		CachedVremCharacter = VremChar;
+
+		CachedVremCharacter->OnStateTagAdded.AddDynamic(this, &UVremAnimInstance::HandleStateTagAdded);
+		CachedVremCharacter->OnStateTagRemoved.AddDynamic(this, &UVremAnimInstance::HandleStateTagRemoved);
+
+		FGameplayTagContainer OwnedTags;
+		CachedVremCharacter->GetOwnedGameplayTags(OwnedTags);
+		for (const FVremAnimTagBinding& Binding : TagBindings)
+		{
+			ApplyTagChange(Binding.Tag, OwnedTags.HasTag(Binding.Tag));
+		}
+	}
+	else
+	{
+		UE_LOG(LogVremInput, Warning, TEXT("UVremAnimInstance::NativeBeginPlay invalid VremChar"));
+	}
+}
+
+void UVremAnimInstance::NativeUninitializeAnimation()
+{
+	if (CachedVremCharacter.Get())
+	{
+		CachedVremCharacter->OnStateTagAdded.RemoveDynamic(this, &UVremAnimInstance::HandleStateTagAdded);
+		CachedVremCharacter->OnStateTagRemoved.RemoveDynamic(this, &UVremAnimInstance::HandleStateTagRemoved);
+	}
+	ResolvedTagProperties.Empty();
+
+	Super::NativeUninitializeAnimation();
+}
+
+void UVremAnimInstance::CacheTagBindingProperties()
+{
+	ResolvedTagProperties.Empty();
+	UClass* AnimClass = GetClass();
+	for (const FVremAnimTagBinding& Binding : TagBindings)
+	{
+		if (!Binding.Tag.IsValid() || Binding.PropertyToEdit.IsNone())
+		{
+			continue;
+		}
+		if (FBoolProperty* BoolProp = FindFProperty<FBoolProperty>(AnimClass, Binding.PropertyToEdit))
+		{
+			ResolvedTagProperties.Add(Binding.Tag, BoolProp);
+		}
+	}
+}
+
+void UVremAnimInstance::ApplyTagChange(const FGameplayTag& Tag, bool bIsAdded)
+{
+	if (FBoolProperty** Found = ResolvedTagProperties.Find(Tag))
+	{
+		(*Found)->SetPropertyValue_InContainer(this, bIsAdded);
+	}
+}
+
+void UVremAnimInstance::HandleStateTagAdded(const FGameplayTag& Tag)
+{
+	ApplyTagChange(Tag, true);
+}
+
+void UVremAnimInstance::HandleStateTagRemoved(const FGameplayTag& Tag)
+{
+	ApplyTagChange(Tag, false);
+}
--- Source/Vrem/Animation/VremAnimInstance.h
+++ Source/Vrem/Animation/VremAnimInstance.h
@@ -4,21 +4,55 @@
 
 #include "CoreMinimal.h"
 #include "Animation/AnimInstance.h"
+#include "GameplayTagContainer.h"
 #include "VremAnimInstance.generated.h"
 
-/**
- * 
- */
+class AVremCharacter;
+
+USTRUCT(BlueprintType)
+struct FVremAnimTagBinding
+{
+    GENERATED_BODY()
+
+    UPROPERTY(EditAnywhere, Category = "Vrem|Animation")
+    FGameplayTag Tag;
+
+    UPROPERTY(EditAnywhere, Category = "Vrem|Animation")
+    FName PropertyToEdit;
+};
+
 UCLASS()
 class VREM_API UVremAnimInstance : public UAnimInstance
 {
 	GENERATED_BODY()
 	
-
 public:
+    virtual void NativeInitializeAnimation() override;
+    virtual void NativeBeginPlay() override;
+    virtual void NativeUninitializeAnimation() override;
+
 	TSubclassOf<UAnimInstance> GetCurrentLayer() const { return CurrentLayer; }
+
+protected:
+    void CacheTagBindingProperties();
+    void ApplyTagChange(const FGameplayTag& Tag, bool bIsAdded);
+    UFUNCTION()
+    void HandleStateTagAdded(const FGameplayTag& Tag);
+    UFUNCTION()
+    void HandleStateTagRemoved(const FGameplayTag& Tag);
 
 protected:
     UPROPERTY(Transient)
     TSubclassOf<UAnimInstance> CurrentLayer;
+
+protected:
+	UPROPERTY(EditDefaultsOnly, Category = "Vrem|Animation|TagBindings")
+    TArray<FVremAnimTagBinding> TagBindings;
+
+	UPROPERTY(BlueprintReadOnly, Category = "Character State Data")
+	float GroundDistance = -1.0f;
+
+private:
+    TWeakObjectPtr<AVremCharacter> CachedVremCharacter;
+    TMap<FGameplayTag, FBoolProperty*> ResolvedTagProperties;
 };
